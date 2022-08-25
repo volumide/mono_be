491113311fa1663afd2e80136670e3299f4f236f
Afowowe Olumide Victor <volumide42@gmail.com>
2022-08-25 21:09:40 +0100

    add database

---
+++ database/createTable.ts
@@ -0,0 +1,27 @@
+import connection from "../connection/connect";
+
+const account = `CREATE TABLE accounts (
+	id int(11) NOT NULL,
+	user_id int(11) NOT NULL,
+	mono_id varchar(255) NOT NULL
+  ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+  `;
+
+const userTable = `
+ CREATE TABLE users(
+	id int(11) NOT NULL,
+	first_name varchar(255) NOT NULL,
+	last_name varchar(255) NOT NULL,
+	email varchar(255) NOT NULL,
+	password varchar(255) NOT NULL
+  ) ENGINE=InnoDB DEFAULT CHARSET=utf8;`;
+
+connection.query(account, (err, res) => {
+	if (err) throw err;
+	console.log("table created");
+});
+
+connection.query(userTable, (err, res) => {
+	if (err) throw err;
+	console.log("table created");
+});
